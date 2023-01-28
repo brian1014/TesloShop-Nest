548fe39a9eb417d5bb5d2215c60e2a7bcb5c809c
Brian Alvarez <brianalvarez2000@gmail.com>
2023-01-28 11:20:01 -0500

    Se creo la relacion (OneToMany Product to  ProductImage) y (ManyToOne ProductImage to Product)

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,8 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe, Query } from '@nestjs/common';
-import { ProductsService } from './products.service';
-import { CreateProductDto } from './dto/create-product.dto';
-import { UpdateProductDto } from './dto/update-product.dto';
-import { PaginationDto } from '../common/dto/pagination.dto';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe, Query } from '@nestjs/common'
+import { ProductsService } from './products.service'
+import { CreateProductDto } from './dto/create-product.dto'
+import { UpdateProductDto } from './dto/update-product.dto'
+import { PaginationDto } from '../common/dto/pagination.dto'
 
 @Controller('products')
 export class ProductsController {
@@ -10,28 +10,28 @@ export class ProductsController {
 
   @Post()
   create(@Body() createProductDto: CreateProductDto) {
-    return this.productsService.create(createProductDto);
+    return this.productsService.create(createProductDto)
   }
 
   @Get()
   findAll(@Query() paginationDto: PaginationDto) {
-    return this.productsService.findAll(paginationDto);
+    return this.productsService.findAll(paginationDto)
   }
 
   @Get(':term')
   findOne(@Param('term') term: string) {
-    return this.productsService.findOne(term);
+    return this.productsService.findOne(term)
   }
 
   @Patch(':id')
   update(
     @Param('id', ParseUUIDPipe) id: string, 
     @Body() updateProductDto: UpdateProductDto) {
-    return this.productsService.update(id, updateProductDto);
+    return this.productsService.update(id, updateProductDto)
   }
 
   @Delete(':id')
   remove(@Param('id', ParseUUIDPipe) id: string) {
-    return this.productsService.remove(id);
+    return this.productsService.remove(id)
   }
 }
